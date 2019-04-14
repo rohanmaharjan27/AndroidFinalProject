b12119ede63550224c87282d107a15c19ac503b4
Rohan Maharjan <rohanmaharjan123@hotmail.com>
2019-04-14 16:33:10 +0545

    created splash screen

---
+++ app/src/main/java/com/rohan/pchardwarestore/SplashScreen.java
@@ -0,0 +1,24 @@
+package com.rohan.pchardwarestore;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        final Intent intent=new Intent(SplashScreen.this,MainActivity.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+    }
+}
