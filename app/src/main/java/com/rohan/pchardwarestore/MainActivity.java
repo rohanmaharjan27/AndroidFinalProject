d9a1e43cb1c760d32beb70e4832d753e67efafcf
Rohan Maharjan <rohanmaharjan123@hotmail.com>
2019-04-29 20:15:36 +0545

    navigation drawer and fragments added

--- app/src/main/java/com/rohan/pchardwarestore/MainActivity.java
+++ app/src/main/java/com/rohan/pchardwarestore/MainActivity.java
@@ -1,13 +1,84 @@
 package com.rohan.pchardwarestore;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar =findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer=findViewById(R.id.draw_layout);
+        NavigationView navigationView=findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(this,drawer,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState==null) { // to only show shop fragment when wanted
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new ShopFragment()).commit();  // to open fragment when app is opened at the start(default fragment)
+            navigationView.setCheckedItem(R.id.nav_shop);
+        }
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.nav_shop:
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ShopFragment()).commit();
+                break;
+
+            case R.id.nav_cart:
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new CartFragment()).commit();
+                break;
+
+            case R.id.nav_wishlist:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new WishlistFragment()).commit();
+                break;
+
+            case R.id.nav_profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new AccountFragment()).commit();
+                break;
+
+            case R.id.nav_about:
+                Toast.makeText(this,"Version 1.1",Toast.LENGTH_SHORT).show();
+
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true; //to select the item after we click it
+    }
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START); //gravitycompat.end for right side drawer
+        }
+        else {
+            super.onBackPressed(); // closes the activity
+        }
+    }
+
+
 }
